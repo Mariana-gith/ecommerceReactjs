0f37a7e8f61eaf48c764ca7ef001badba03509db
Mariana-gith <mariana.a.iamarino@gmail.com>
2021-08-20 01:09:03 -0300

    creacion de barra de navegacion

---
+++ src/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar (){
+    return(
+        <div>
+            <p>Hola</p>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import Container from "react-bootstrap/Container"
+
+function NavBar() {
+    return(
+        <div>
+           <Navbar bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home">Musilandia</Navbar.Brand>
+                <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Tieneda</Nav.Link>
+                <Nav.Link href="#pricing">Contacto</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+ 
+
+        </div>
+    )
+}
+
+export default NavBar 
