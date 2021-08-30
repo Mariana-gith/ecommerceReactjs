747768f5c496c56a2b60e65946331e5b98a2f064
Mariana-gith <mariana.a.iamarino@gmail.com>
2021-08-30 13:16:07 -0300

    creo estilo css

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import './App.css';
 import NavBar from './componentes/NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Bienvenida from './componentes/ItemListConteiner/Bienvenida'
+import Contador from './componentes/ItemCount/Contador';
 
 
 
 
 function App() {
+
+  const onAdd = (count)=> {
+    alert( `Compraste ${count} productos`)     
+  }
+
   return (
-    <div className="App">
-      <NavBar/>  
-        <div>
-          <Bienvenida/>
-        </div>  
-    </div>  
+    <> 
+      <div className="App">
+          <NavBar/>  
+          <div>
+            <Bienvenida/>
+          </div> 
+          <Contador initial={1} stock={10} onAdd={onAdd} /> 
+      </div>
+    </>
   );
 }
 
---
+++ src/componentes/ItemCount/Contador.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import './index.scss';
+
+const Contador = ({initial, stock, onAdd})=> {
+
+    const [count , setCount] = useState (initial)
+
+    const handlerSumar = ()=> {
+       if (count === 10){
+           alert('stock no diponible')
+       }else{
+        setCount(count +1)}
+    }
+    const handlerRestar = ()=> {
+       if (count > initial){
+        setCount(count -1)}
+    } 
+
+    const handlerReset = ()=> {
+        setCount(initial)        
+    }
+
+    const handlerOnAdd = ()=> {
+        onAdd(count)
+      
+    }
+
+    return(
+        <div>
+            <button onClick={handlerSumar}>+</button>
+            <label>  {count}  </label>
+            <button onClick={handlerRestar}>-</button> <br/>
+            <button className = "btn btn-outline-primary btn-block" onClick={handlerOnAdd}>Add</button>
+            <button className = "btn btn-outline-primary btn-block" onClick={handlerReset}>Reset</button>
+
+        </div>
+    )
+}
+
+
+export default Contador 
